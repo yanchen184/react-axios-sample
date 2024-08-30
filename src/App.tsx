f062a77241872f5203e15bd37f32a7d2c937cd43
TW - IT - YC Chen <yan.chen@shalter.com>
2024-08-30 17:23:44 +0800

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,55 @@
 // src/App.tsx
 
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import Home from "./components/Home";
 import About from "./components/About";
 import Example from "./components/Example";
 import Whisper from "./components/Whisper";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/example">Example</Link>
-            </li>
-            <li>
-              <Link to="/whisper">Whisper</Link>
-            </li>
-          </ul>
-        </nav> */}
+  useEffect(() => {
+    // 初始化 adsbygoogle，只在第一次掛載時執行
+    const loadAds = () => {
+      if (window.adsbygoogle) {
+        try {
+          window.adsbygoogle.push({});
+        } catch (e) {
+          console.error("Adsbygoogle error:", e);
+        }
+      }
+    };
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/example" element={<Example />} />
-          <Route path="/whisper" element={<Whisper />} />
-        </Routes>
-      </div>
-    </Router>
+    loadAds();
+
+    return () => {
+      // Optional: 清理函式，例如重置 adsbygoogle 狀態
+      const ads = document.querySelectorAll(".adsbygoogle");
+      ads.forEach((ad) => ad.remove());
+    };
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <Router>
+        <Helmet>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7491649138626528"
+            crossorigin="anonymous"
+          ></script>
+        </Helmet>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/example" element={<Example />} />
+            <Route path="/whisper" element={<Whisper />} />
+          </Routes>
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 };
 
